7e40e208f8451cd90f5f4471e54cafd678dffcf1
Alex Demishkevich <demalex@me.com>
2018-01-22 22:12:22 +0200

    Add dependencies updating cron task configuration

---
+++ scripts/.update.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Updating AppStore dependencies..."
+
+mas outdated && mas upgrade
+
+echo "Updating brew formulas..."
+brew update
+
+echo "Updating brew dependencies..."
+cd ~/ && brew bundle install --global --verbose
+
+echo "Updating gem manager..."
+gem update --system
+
+echo "Updating gems..."
+gem update
+cd ~/ && bundle install --gemfile=.Gemfile
